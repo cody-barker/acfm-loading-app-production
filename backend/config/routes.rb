ea5d1ee37ca2c3d70aaba79dab73bee56980eb0e
Cody <codybarker.or@gmail.com>
2025-01-27 10:43:04 -0800

    namespace api backend routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  resources :loading_list_items
-  resources :items
-  resources :loading_lists
-  resources :teams
-  resources :users
+  
+  namespace :api do
+    resources :loading_list_items
+    resources :items
+    resources :loading_lists
+    resources :teams
+    resources :users
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
