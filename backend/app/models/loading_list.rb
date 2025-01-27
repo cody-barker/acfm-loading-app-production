d94131765fcf55bec50d2543e5d5b05777794565
Cody <codybarker.or@gmail.com>
2025-01-27 11:00:58 -0800

    add loading_list and team model relationships and validations

--- backend/app/models/loading_list.rb
+++ backend/app/models/loading_list.rb
@@ -1,2 +1,7 @@
 class LoadingList < ApplicationRecord
+  belongs_to :user
+  belongs_to :team
+  has_many :loading_list_items
+  validates :date, :return_date, :site_name, presence: true
+  validates :site_name, length: { minimum: 3, maximum: 50 }
 end
--- backend/app/models/team.rb
+++ backend/app/models/team.rb
@@ -1,2 +1,4 @@
 class Team < ApplicationRecord
+  has_many :loading_lists
+  validates :name, presence: true, length: {minimum: 1, maximum: 50}
 end
