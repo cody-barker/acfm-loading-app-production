1d1f7be4c0c5df1a3bba18f506b77579053a7216
Cody <codybarker.or@gmail.com>
2025-01-27 10:26:11 -0800

    remove loading_list foreign key from user table

--- backend/db/migrate/20250127182343_create_users.rb
+++ backend/db/migrate/20250127182343_create_users.rb
@@ -1,7 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[7.2]
   def change
     create_table :users do |t|
-      t.integer :loading_list_id
       t.string :email
       t.string :password_digest
       t.string :first_name
