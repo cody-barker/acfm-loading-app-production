e5272a81ee41b77d2ac35d7fe816818448e2051e
Cody <codybarker.or@gmail.com>
2025-01-27 10:52:58 -0800

    add cookie and session config to application.rb

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -8,8 +8,16 @@ Bundler.require(*Rails.groups)
 
 module Backend
   class Application < Rails::Application
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+    
+    # Use SameSite=Strict for all cookies to help protect against CSRF
+    # https://owasp.org/www-community/SameSite
+    config.action_dispatch.cookies_same_site_protection = :strict
+    
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
+    
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
