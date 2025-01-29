fa9290a2ba777855df8f5e56c7b754dd9cf1d581
Cody <codybarker.or@gmail.com>
2025-01-28 20:12:40 -0800

    correct model associations in loadinglistitem serializer to singular

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
-  
-  namespace :api do
+
+
     resources :loading_list_items
     resources :items
     resources :loading_lists
     resources :teams
     resources :users
-  end
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
 
   # Defines the root path route ("/")
   # root "posts#index"
