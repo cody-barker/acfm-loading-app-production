ebd01e19799da3c3176a606739573d982c639ed5
Cody <codybarker.or@gmail.com>
2025-01-28 20:01:04 -0800

    fill out controllers

--- backend/app/controllers/loading_list_items_controller.rb
+++ backend/app/controllers/loading_list_items_controller.rb
@@ -1,2 +1,41 @@
 class LoadingListItemsController < ApplicationController
+  def index 
+    loading_list_items = LoadingListItem.all
+    render json: loading_list_items
+  end
+
+  def show
+    loading_list_item = LoadingListItem.find(params[:id])
+    render json: loading_list_item
+  end
+
+  def create
+    loading_list_item = LoadingListItem.new(loading_list_item_params)
+    if loading_list_item.save
+      render json: loading_list_item, status: :created
+    else
+      render json: loading_list_item.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    loading_list_item = LoadingListItem.find(params[:id])
+    if loading_list_item.update(loading_list_item_params)
+      render json: loading_list_item
+    else
+      render json: loading_list_item.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    loading_list_item = LoadingListItem.find(params[:id])
+    loading_list_item.destroy
+    render json: {}, status: :no_content
+  end
+
+  private
+
+  def loading_list_item_params
+    params.require(:loading_list_item).permit(:loading_list_id, :item_id)
+  end
 end
--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: users
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:email, :password_digest, :first_name, :last_name, :role)
+  end 
 end
