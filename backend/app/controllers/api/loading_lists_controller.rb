606f560c770714ac5479dfdc471b3b3170bb2926
Cody <codybarker.or@gmail.com>
2025-01-30 11:34:21 -0800

    update loading_lists_controller

--- backend/app/controllers/api/loading_lists_controller.rb
+++ backend/app/controllers/api/loading_lists_controller.rb
@@ -1,43 +1,38 @@
   class Api::LoadingListsController < ApplicationController
-    
+    before_action :set_loading_list, only: [:show, :update, :destroy]
+
     def index 
-      loading_lists = LoadingList.all
+      loading_lists = LoadingList.includes(:user, :team, :loading_list_items => :item).all
       render json: loading_lists
     end
 
     def show
-      loading_list = LoadingList.find(params[:id])
-      render json: loading_list
+      render json: @loading_list
     end
 
     def create
-      loading_list = LoadingList.new(loading_list_params)
-      if loading_list.save
-        render json: loading_list, status: :created
-      else
-        render json: loading_list.errors, status: :unprocessable_entity
-      end
+      loading_list = LoadingList.create!(loading_list_params)
+      render json: loading_list, status: :created
     end
 
     def update
-      loading_list = LoadingList.find(params[:id])
-      if loading_list.update(loading_list_params)
-        render json: loading_list
-      else
-        render json: loading_list.errors, status: :unprocessable_entity
-      end
+      @loading_list.update!(loading_list_params)
+      render json: @loading_list
     end
 
     def destroy
-      loading_list = LoadingList.find(params[:id])
-      loading_list.destroy
-      render json: {}, status: :no_content
+      @loading_list.destroy
+      head :no_content
     end
 
     private
 
+    def set_loading_list
+      @loading_list = LoadingList.includes(:user, :team, :loading_list_items => :item).find(params[:id])
+    end
+
     def loading_list_params
-      params.require(:loading_list).permit(:name, :description)
+      params.require(:loading_list).permit(:user_id, :team_id, :date, :return_date, :site_name, :notes)
     end
 
 end
