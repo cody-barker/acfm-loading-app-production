550447c9e53babc6c0aa917e3284496672a90a2c
Cody <codybarker.or@gmail.com>
2025-01-30 11:08:19 -0800

    copy favicon.ico from client into backend/public to avoid favicon.ico GET errors on http requests, and eager load item responses

--- backend/app/controllers/api/items_controller.rb
+++ backend/app/controllers/api/items_controller.rb
@@ -1,43 +1,45 @@
 class Api::ItemsController < ApplicationController
+  before_action :set_item, only: %i[show update destroy]
 
-    def index
-      items = Item.all
-      render json: items
+  def index
+    items = Item.includes(:loading_list_items).all
+    render json: items
+  end
+
+  def show
+    render json: @item
+  end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      render json: item, status: :created
+    else
+      render json: item.errors, status: :unprocessable_entity
     end
+  end
 
-    def show
-      item = Item.find(params[:id])
-      render json: item
+  def update
+    if @item.update(item_params)
+      render json: @item
+    else
+      render json: @item.errors, status: :unprocessable_entity
     end
+  end
 
-    def create
-      item = Item.new(item_params)
-      if item.save
-        render json: item, status: :created
-      else
-        render json: item.errors, status: :unprocessable_entity
-      end
-    end
+  def destroy
+    @item.destroy
+    head :no_content 
+  end
 
-    def update
-      item = Item.find(params[:id])
-      if item.update(item_params)
-        render json: item
-      else
-        render json: item.errors, status: :unprocessable_entity
-      end
-    end
+  private
 
-    def destroy
-      item = Item.find(params[:id])
-      item.destroy
-      render json: {}, status: :no_content
-    end
+  def set_item
+    @item = Item.includes(:loading_list_items).find(params[:id])
+  end
 
-    private
 
-    def item_params
-      params.require(:item).permit(:name, :category, :quantity)
-    end 
-
+  def item_params
+    params.require(:item).permit(:name, :category, :quantity)
+  end
 end
