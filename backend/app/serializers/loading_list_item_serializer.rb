19fcf0648a4b1447055baee50bf9fe7951203367
Cody <codybarker.or@gmail.com>
2025-01-30 14:07:06 -0800

    comment out code for manually mapping items under loading_list_items. instead plan to fetch items separately and decouple the data

--- backend/app/serializers/loading_list_item_serializer.rb
+++ backend/app/serializers/loading_list_item_serializer.rb
@@ -1,5 +1,5 @@
 class LoadingListItemSerializer < ActiveModel::Serializer
   attributes :id, :loading_list_id, :item_id, :quantity
-  belongs_to :loading_list
-  belongs_to :item, serializer: ItemSerializer
+  # belongs_to :loading_list
+  belongs_to :item
 end
