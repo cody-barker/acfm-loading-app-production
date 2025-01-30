29d619ccc7aab6c2ae58f66304f4f287289c95d5
Cody <codybarker.or@gmail.com>
2025-01-30 11:49:48 -0800

    update serializers, attempting to nest item with loading_list_item but its not working

--- backend/app/serializers/loading_list_serializer.rb
+++ backend/app/serializers/loading_list_serializer.rb
@@ -2,4 +2,5 @@ class LoadingListSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :team_id, :site_name, :date, :return_date, :notes
   belongs_to :user
   belongs_to :team
+  has_many :loading_list_items, serializer: LoadingListItemSerializer
 end
