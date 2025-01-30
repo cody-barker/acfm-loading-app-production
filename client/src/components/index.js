87e4491ca20e2e27e7ff76a864ea1f8626278e9d
Cody <codybarker.or@gmail.com>
2025-01-30 15:35:42 -0800

    wrap root in context providers and browser router

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from '../reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "../reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from "./contexts/UserContext";
+import { ItemsProvider } from "./contexts/ItemsContext";
+import { TeamsProvider } from "./contexts/TeamsContext";
+import { LoadingListsProvider } from "./contexts/LoadingListsContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <UserProvider>
+      <ItemsProvider>
+        <TeamsProvider>
+          <LoadingListsProvider>
+            <App />
+          </LoadingListsProvider>
+        </TeamsProvider>
+      </ItemsProvider>
+    </UserProvider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
