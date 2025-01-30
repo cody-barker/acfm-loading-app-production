b6e8baf237d92ee0688e3959c5876b27a1c07a1f
Cody <codybarker.or@gmail.com>
2025-01-30 11:36:33 -0800

    update teams controller

--- backend/app/controllers/api/teams_controller.rb
+++ backend/app/controllers/api/teams_controller.rb
@@ -1,34 +1,35 @@
 class Api::TeamsController < ApplicationController
+  before_action :set_team, only: %i[show update destroy]
 
   def index
-    teams = Team.all
+    teams = Team.includes(:loading_lists).all
     render json: teams
   end 
 
   def show
-    team = Team.find(params[:id])
-    render json: team
+    render json: @team
   end 
 
   def create
-    team = Team.new(team_params)
-    if team.save
-      render json: team, status: :created
-    else
-      render json: team.errors, status: :unprocessable_entity
-    end
+    team = Team.create!(team_params)
+    render json: team, status: :created
   end
 
   def update
-    team = Team.find(params[:id])
-    if team.update(team_params)
-      render json: team
-    else
-      render json: team.errors, status: :unprocessable_entity
-    end
+    @team.update!(team_params)
+    render json: @team
+  end
+
+  def destroy
+    @team.destroy
+    head :no_content
   end
 
   private
+
+  def set_team
+    @team = Team.includes(:loading_lists).find(params[:id])
+  end
 
   def team_params
     params.require(:team).permit(:name)
