49eee1334e69ab44706d3d432361f16e37f8a71d
Cody <codybarker.or@gmail.com>
2025-01-31 10:45:41 -0800

    change login title to acfm loading app

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,7 +4,7 @@ function Login() {
   return (
     <div className="wrapper  background-image ">
       <div className="wrapper__form wrapper__form--login">
-        <h1 className="wrapper__title">ACFM Pesticide Inventory</h1>
+        <h1 className="wrapper__title">ACFM Loading App</h1>
         <LoginForm />
       </div>
     </div>
